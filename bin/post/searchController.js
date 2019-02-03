8d341148250f0d242f12e5fe8a3abe3f0a1a08ea
samuvelp <samuvelpandian@gmail.com>
2019-02-03 23:54:31 +0530

    completed link insertion logic

--- bin/post/searchController.js
+++ bin/post/searchController.js
@@ -1,6 +1,9 @@
 const scraper = require(`google-search-scraper`)
+const deathByCaptcha = require('deathbycaptcha')
+const dbc = new deathByCaptcha('username','password')
 const Pool = require(`../../dbcredentials`).pool
 const pool = Pool()
+
 
 const searchLinkForTopic = ((topic, userId) => {
     const links = []
@@ -8,15 +11,19 @@ const searchLinkForTopic = ((topic, userId) => {
         query: '"${topic} site:medium.com"',
         limit: 10,
         age: 'y',
+        solver:dbc,
         params: { query: "what is npm" }
     }
 
     const promise = new Promise((resolve, reject) => {
         scraper.search(options, (err, url, meta) => {
             try {
+                console.log(err)
                 console.log(url)
-                links.push(url)
-                resolve(links, userId)
+                // if (url != undefined) {
+                //     links.push(url)
+                //     resolve(links, userId)
+                // }
             } catch (err) {
                 console.log(err)
                 reject("Server error")
@@ -24,26 +31,43 @@ const searchLinkForTopic = ((topic, userId) => {
         })
     })
     promise.then(() => {
-        checkAndCreateLinkRecord(links, topic, userId)
-    }).catch(() => {
-        console.log()
+        checkAndCreateLinkRecord(links, 0, topic, userId)
+    }).catch((err) => {
+        console.log(err)
     })
 })
 
-const checkAndCreateLinkRecord = (links, topic, userId) => {
+const checkAndCreateLinkRecord = (links, linkIndex, topic, userId) => {
     new Promise((resolve, reject) => {
-        for (var link of links) {
-            if (!isLinkInDB(userId, link)) {
-                createRecord(userId, link)
-                reject("new link created")
-                break;
-            }
-        }
-    }).then(() => {
-        superSearchLinkForTopic(topic, userId)
-    }).catch(() => {
-
+        isLinkInDB(userId, links[linkIndex])
+            .then((value) => {
+                console.log(links.length, linkIndex)
+                console.log(value, "inside islinkindb promise")
+                if (!value) {
+                    console.log("inside false scope")
+                    createRecord(userId, links[linkIndex])
+                    reject("new link created")
+                } else if (value && (links.length > linkIndex)) {
+                    resolve(true)
+                    //recurse to check the rest of the links if available
+                } else {
+                    resolve(false)
+                    //else do the super special link query search
+                }
+            })
     })
+        .then((result) => {
+            console.log(result)
+            if (result) {
+                checkAndCreateLinkRecord(links, linkIndex + 1, topic, userId)
+            } else {
+                // superSearchLinkForTopic(topic, userId)
+                console.log("main super search executed")
+            }
+        })
+        .catch((err) => {
+            console.log(err)
+        })
 }
 
 const superSearchLinkForTopic = (topic, userId) => {
@@ -52,21 +76,23 @@ const superSearchLinkForTopic = (topic, userId) => {
 }
 
 const isLinkInDB = (userId, link) => {
-    console.log('isLinkInDB()', 'link:', link, 'userid:', userId)
-    pool.query(`select * from learninglinks where userid = $1 and link = $2`, [userId, link], (error, results) => {
-        if (results.rowCount === 0) {
-            console.log(false)
-            return false
-        } else {
-            console.log(false)
-            return true
-        }
+    return new Promise((resolve, reject) => {
+        console.log('isLinkInDB()', 'link:', link, 'userid:', userId)
+        pool.query(`select * from learninglinks where userid = $1 and link = $2`, [userId, link], (error, results) => {
+            if (results.rowCount === 0) {
+                console.log(false)
+                resolve(false)
+            } else {
+                console.log(true)
+                resolve(true)
+            }
+        })
     })
 }
 
 const createRecord = (userId, link) => {
     console.log('createRecord()', 'userid:', userId, 'link:', link)
-    pool.query('insert into learninglinks (userid,link) values($1,$2)', [userId, link], (error, results) => {
+    pool.query('insert into learninglinks (userid,link,createdat,lastsent,sessions) values($1,$2,$3,$4,$5)', [userId, link, new Date().getTime()], (error, results) => {
         try {
             console.log(`Inserted into learninglink for userid :$1 with link:$2`, [results.userid, results.link])
         } catch (error) {
