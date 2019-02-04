57d20ed9f3167bbe5643dc2219dd5cf695939a46
samuvelp <samuvelpandian@gmail.com>
2019-02-04 22:34:35 +0530

    passing response to searchLinkForTopic function

--- bin/post/topicSubscribeHandler.js
+++ bin/post/topicSubscribeHandler.js
@@ -4,7 +4,8 @@ const subscribeToTopic = (request, response) => {
     // const userid = request.params.uuid
     // const topic = request.params.topic
     // const searchAge=request.params.searchAge (latest/alltime)
-    searchController.searchLinkForTopic("nodejs","550e8400-e29b-41d4-a716-446655440034")
+    searchController.searchLinkForTopic("nodejs","550e8400-e29b-41d4-a716-446655440034",response)
+    
 }
 module.exports = {
     subscribeToTopic
