6b71b3c127ad20c0630dc7cae20e35f5dd27f51a
samuvelp <samuvelpandian@gmail.com>
2019-02-09 01:03:21 +0530

    removed token

--- bin/post/pushNotification.js
+++ bin/post/pushNotification.js
@@ -1,6 +1,6 @@
 const admin = require("firebase-admin");
 const serviceAccount = require("/home/samuvel/Documents/apidev/microLearning/fb-admin.json");
-const deviceToken = 'ffmllcu7MWM:APA91bFANTR4GyOTlTLED7ApvCLdBL5D_DKDysfsuRvvAzBeIMxAHVj_-UufvM2p4I5pe9Yj9tBP65nLk1w3b8nqTnTy73wZwdK7ITqdKN75ZQ3Umj9vFsOY_liWl1uwoNfKXp_2jKjy'
+const deviceToken = ''
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://micro-learning-2f309.firebaseio.com"
