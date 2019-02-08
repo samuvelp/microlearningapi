9258e7ec83e0794cfece0f9f0f159a75bb641b9a
samuvelp <samuvelpandian@gmail.com>
2019-02-09 00:45:19 +0530

    git ignore updated for fb admin file

---
+++ bin/post/schedular.js
@@ -0,0 +1,5 @@
+//logic
+//new table to manage sub list - done
+// table contains column userId, subscribed topic, sub date and device token. -done
+//get the latest sub topic and do the cron job for the user;)
+//end result of the job is to send url as push notification, save the url in table -> learninglinks,
