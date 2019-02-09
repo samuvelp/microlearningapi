2e417d0d789b03e3ea167983e5c654d02d939112
samuvelp <samuvelpandian@gmail.com>
2019-02-10 01:35:26 +0530

    node-cron added to project

--- bin/post/schedular.js
+++ bin/post/schedular.js
@@ -1,5 +1,21 @@
-//logic
-//new table to manage sub list - done
-// table contains column userId, subscribed topic, sub date and device token. -done
-//get the latest sub topic and do the cron job for the user;)
-//end result of the job is to send url as push notification, save the url in table -> learninglinks,
+const cron = require('node-cron')
+const Pool = require('../../dbcredentials').pool
+const pool = Pool()
+
+cron.schedule(`39 23 * * *`, () => {
+    console.log('Running evert 5 sec dude')
+})
+const subscribe = (topic, userId, deviceToken, response) => {
+
+}
+const storeUserSubscription = (topic, userId, deviceToken) => {
+    pool.query(`INSERT INTO subscription (userid,devicetoken,topic,isactive,subscribedat) VALUES($1,$2,$3,$4,$5)`,
+        [userId,deviceToken,topic,true,new Date().getTime()]
+        ,(error,results)=>{
+            if(error){
+                console.log(error,"subscription insert failed")
+            }else{
+                console.log("Subscribed for userid:".userId,"Success!")
+            }
+        })
+}
