b4af5a6bb628f4fd24fdc76483ef20125d4cef6a
samuvelp <samuvelpandian@gmail.com>
2019-02-10 01:35:01 +0530

    notification handler updated

--- bin/post/pushNotification.js
+++ bin/post/pushNotification.js
@@ -1,24 +1,33 @@
 const admin = require("firebase-admin");
 const serviceAccount = require("/home/samuvel/Documents/apidev/microLearning/fb-admin.json");
-const deviceToken = ''
+
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://micro-learning-2f309.firebaseio.com"
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://micro-learning-2f309.firebaseio.com"
 });
-const payload = {
-    data:{
-        title : 'First message',
-        body : 'Oh yheaaaaa!',
-        url : 'www.google.com'
+
+
+const sendNotification = (deviceToken, payloadData) => {
+    const options = {
+        priority: "high"
     }
+
+    const payload = {
+        data: {
+            title: payloadData.title,
+            body: payloadData.body,
+            url: payloadData.url
+        }
+    }
+    
+    admin.messaging().sendToDevice(deviceToken, payload, options)
+        .then((response) => {
+            console.log('Successfully sent notification', response)
+        })
+        .catch((error) => {
+            console.log('Failed to send notification', error)
+        })
 }
-const options = {
-    priority: "high"
+module.exports ={
+    sendNotification
 }
-admin.messaging().sendToDevice(deviceToken,payload,options)
-    .then((response)=>{
-        console.log('Successfully sent notification',response)
-    })
-    .catch((error)=>{
-        console.log('Failed to send notification',error)
-    })
