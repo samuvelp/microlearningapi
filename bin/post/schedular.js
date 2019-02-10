f33717a5ce66eddf9766c6fb93999e48919ac0a3
samuvelp <samuvelpandian@gmail.com>
2019-02-10 11:21:55 +0530

    schedular updated

--- bin/post/schedular.js
+++ bin/post/schedular.js
@@ -1,21 +1,26 @@
 const cron = require('node-cron')
 const Pool = require('../../dbcredentials').pool
+const searchController = require('./searchController')
 const pool = Pool()
 
-cron.schedule(`39 23 * * *`, () => {
-    console.log('Running evert 5 sec dude')
-})
-const subscribe = (topic, userId, deviceToken, response) => {
 
+const subscribe = (topic, userId, deviceToken, response) => {
+    storeUserSubscription(topic, userId, deviceToken, response)
 }
-const storeUserSubscription = (topic, userId, deviceToken) => {
+const storeUserSubscription = (topic, userId, deviceToken, response) => {
     pool.query(`INSERT INTO subscription (userid,devicetoken,topic,isactive,subscribedat) VALUES($1,$2,$3,$4,$5)`,
-        [userId,deviceToken,topic,true,new Date().getTime()]
-        ,(error,results)=>{
-            if(error){
-                console.log(error,"subscription insert failed")
-            }else{
-                console.log("Subscribed for userid:".userId,"Success!")
+        [userId, deviceToken, topic, true, new Date().getTime()]
+        , (error, results) => {
+            if (error) {
+                console.log(error, "subscription insert failed")
+            } else {
+                console.log("Subscribed for userid:".userId, "Success!")
+                //immediate search response for first time subscribers
+                searchController.searchLinkForTopic(topic, userId, response)
+                //cron job to run every day at 8:30 am of locale //todo find local time of the user
+                cron.schedule(`30 08 * * *`, () => {
+                    searchController.searchLinkForTopic(topic, userId, response)
+                })
             }
         })
 }
