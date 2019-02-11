aa91c8700b73ad3985b8bf3129ced4e031db8a7d
samuvelp <samuvelpandian@gmail.com>
2019-02-11 22:21:51 +0530

    local dir removed

--- bin/post/pushNotification.js
+++ bin/post/pushNotification.js
@@ -1,5 +1,6 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("/home/samuvel/Documents/apidev/microLearning/fb-admin.json");
+// const serviceAccount = require("/home/samuvel/Documents/apidev/microLearning/fb-admin.json");
+const serviceAccount = require(`../../fb-admin.json`)
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
